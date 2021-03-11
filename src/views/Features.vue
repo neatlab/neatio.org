46ca8a663568d6bbe1b12874755dffa88d427d0d
tiz-ul <77936472+tiz-ul@users.noreply.github.com>
2021-03-11 22:49:58 +0200

    Added files

---
+++ src/views/Features.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="view-container">
+        <div class="section section--spacing-bottom">
+    </div>
+
+    
+    <section id="neatkey"></section>
+    <div class="header-section__features">{{ $t("DISCOVER NEAT FEATURES") }} </div>
+  
+    
+    <!-- Feature 1 -->
+
+    <div class="section">
+      <div
+        class="section__content section__content--thin-top content-container"
+      >
+        <div class="dual-sections dual-sections--reverse">
+          <div class="dual-sections__info">
+            <h2 class="dual-sections__title">
+              {{ $t("pages.about.feature1Title") }}
+            </h2>
+            <p>{{ $t("pages.about.feature1SubTitle") }}</p>
+            <p>{{ $t("pages.about.feature1Description") }}</p>
+            
+          </div>
+          <div class="dual-sections__image dual-sections__image--sm">
+            <img src="../assets/images/rocket_img.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+    <!-- .section end -->
+    
+    <!--Feature 2 -->
+
+    <div class="section">
+      <div class="section__content content-container">
+        <div class="dual-sections">
+          <div class="dual-sections__info">
+            <h2 class="dual-sections__title">
+              {{ $t("pages.about.feature2Title") }}
+            </h2>
+            <p>{{ $t("pages.about.feature2SubTitle") }}</p>
+            <p>{{ $t("pages.about.feature2Desc") }}</p>
+          </div>
+          <div class="dual-sections__image dual-sections__image--med">
+            <img src="../assets/images/secure_img.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <!-- .section end -->
+
+    <!--Feature 3 -->
+
+    <div class="section">
+      <div
+        class="section__content section__content--thin-top content-container"
+      >
+        <div class="dual-sections dual-sections--reverse">
+          <div class="dual-sections__info">
+            <h2 class="dual-sections__title">
+              {{ $t("pages.about.feature3Title") }}
+            </h2>
+            <p>{{ $t("pages.about.feature3SubTitle") }}</p>
+            <p>{{ $t("pages.about.feature3Desc") }}</p>
+            
+          </div>
+          <div class="dual-sections__image dual-sections__image--sm">
+            <img src="../assets/images/sclb_img.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- END -->
+
+    <!--FEATURE 4 -->
+
+    <div class="section">
+      <div class="section__content content-container">
+        <div class="dual-sections">
+          <div class="dual-sections__info">
+            <h2 class="dual-sections__title">
+              {{ $t("pages.about.feature4Title") }}
+            </h2>
+            <p>{{ $t("pages.about.feature4SubTitle") }}</p>
+            <p>{{ $t("pages.about.feature4Description") }}</p>
+          </div>
+          <div class="dual-sections__image dual-sections__image--med">
+            <img src="../assets/images/eco_img.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- END -->
+
+    <!-- FEATURE 5 -->
+
+        <div class="section">
+      <div
+        class="section__content section__content--thin-top content-container"
+      >
+        <div class="dual-sections dual-sections--reverse">
+          <div class="dual-sections__info">
+            <h2 class="dual-sections__title">
+              {{ $t("pages.about.feature5Title") }}
+            </h2>
+            <p>{{ $t("pages.about.feature5SubTitle") }}</p>
+            <p>{{ $t("pages.about.feature5Description") }}</p>
+            
+          </div>
+          <div class="dual-sections__image dual-sections__image--sm">
+            <img src="../assets/images/ufr_img.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- .section end -->
+
+    <div class="view-container">
+        <div class="section section--spacing-bottom"></div>
+    </div>
+
+  </div>
+</template>
